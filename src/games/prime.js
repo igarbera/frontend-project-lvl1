f51f40d9a483bd9c5b44a3d7e3c1d2d77ea76411
Your Name <you@example.com>
2021-05-11 22:22:33 +0300

    brain-prime

---
+++ src/games/prime.js
@@ -0,0 +1,28 @@
+import gameEngine from '../index.js';
+
+const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".\n';
+
+// определяем простое ли число
+function isPrime(num) {
+  let flag = true;
+  for (let i = 2; i < num; i++) {
+    if (num % i == 0) {
+      flag = false;
+      break; // выйдем из цикла
+    }
+  }
+  return flag;
+}
+
+// функция раунда игры
+const gameFunc = () => {
+  const a = Math.floor(Math.random() * 100);
+
+  // возвращаем обьект с вопросом и корректным ответом
+  return {
+    question: `${a}`,
+    correctAnswer: isPrime(a) ? 'yes' : 'no',
+  };
+};
+
+export default () => gameEngine(rule, gameFunc);
